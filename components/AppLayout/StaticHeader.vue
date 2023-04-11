e8e01b4325474b1b547aa2a64c62e17dba069f92
jshaw990 <jayden.shaw@gmail.com>
2023-04-11 11:03:24 -0600

    Working on navbar

---
+++ components/AppLayout/StaticHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar flat>
+    <nuxt-link to="/">
+      <v-app-bar-title>
+        <div class="text-uppercase">
+          YYC Nerf Wars
+        </div>
+        <div class="xsmall-text ml-4">
+          A Calgary Nerf Club
+        </div>
+      </v-app-bar-title>
+    </nuxt-link>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'StaticHeader',
+  setup () {
+
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
   <v-app dark>
     <v-main>
+      <StaticHeader />
       <Nuxt />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import StaticHeader from '@/components/AppLayout/StaticHeader.vue'
+
 export default {
-  name: 'DefaultLayout'
+  name: 'DefaultLayout',
+  components: {
+    StaticHeader
+  }
 }
 </script>
